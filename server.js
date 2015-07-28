797a37631dc7134a264f456fa8cf70ab5d804cbc
mattsgm <1379tech@gmail.com>
2015-07-28 00:52:12 -0500

    Add DirectLink. Allow users to give out info without showing it in plaintext

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var validator = require('validator');
 //Let's start Express
 var app = express();
 
-//Let body parse our document
+//Let's body parse our document
 app.use(bodyparser.urlencoded({extended: false}));
 
 //Set View Engine
@@ -29,42 +29,7 @@ var db = new sqlite3.Database(db_file);
 app.get('/', function (req,res) {
   var msg = req.query.msg || null;
 
-
 	res.render('pages/index', {output: null, msg: msg});
-});
-
-//Accept index info
-app.post('/', function (req,res) {
-
-//User Defined Vars
-  var ipParts = req.body.ip.split(":");
-  var password = req.body.password;	
-
-console.log( validator.isIP(ipParts, 4) );
-
-//Was that IP REALLY an IP?
-if ( !validator.isIP(ipParts[0], 4) ) {
-	res.redirect('/?msg=Invalid%20IP%20address!');
-	return;
-}  
-
-  //ipParts[0] is always base IP
-  //ipParts [1] is port if provided. Otherwise 27015	
-  var ip = ipParts[0];
-  var port = ipParts[1] || 27015;
-
-//Server Defined Vars
-  var createdOn = new Date().getTime() / 1000 >> 0; //in ms. convert to s. bit shift to drop float val.
-  var uniqueKey = randomstring.generate(7);
-
-  var pass_to_db = [ip, port, password, uniqueKey, createdOn];
-
-  db.run("INSERT INTO links (ip,port,password,uniqueKey,createdOn) VALUES(?,?,?,?,?)", pass_to_db, function (err) {
-
-    var fullLink = "steam://connect/" + ip + ":" + port + "/" + password;
-
-    res.render('pages/index', {output: fullLink, permalink: uniqueKey, msg: null});
-  });
 });
 
 //GET "my link" by token
@@ -81,21 +46,63 @@ app.get('/go', function (req,res) {
       //It does? Let's give them their link back.
       var fullLink = "steam://connect/" + row.ip + ":" + row.port + "/" + row.password;
 
-      res.render('pages/go', {output: fullLink});
+      res.render('pages/go', {output: fullLink, uniqueKey: uniqueKey});
   }
   });
 });
 
+//Given the unique key, immediately redirect user to the steam:// protocol. This allows you to hide your information when giving it to others.
+app.get('/direct', function (req,res) {
+  //Assign key from query
+  var uniqueKey = req.query.uniqueKey;
 
+  //Look it up in the db
+  db.get('SELECT * FROM links WHERE uniqueKey = ?', uniqueKey, function (err, row) {
+    //Does it exist?
+    if(!row) { 
+      res.send("Entry not found");
+      } else {
+      //It does? Let's give them their link back.
+      var fullLink = "steam://connect/" + row.ip + ":" + row.port + "/" + row.password;
 
+      res.redirect(fullLink);
+  }
+  });
+});
 
+//Accept index info
+app.post('/', function (req,res) {
+//User Defined Vars
+  var ipParts = req.body.ip.split(":");
+  var password = req.body.password;	
 
-//test func for db output
-app.get('/db', function (req,res) {
-    db.all('SELECT * FROM links', function (err, rows) {
-        res.render('pages/db', { entries: rows });
-    });
+//Was that IP REALLY an IP?
+  if ( !validator.isIP(ipParts[0], 4) ) {
+	  res.redirect('/?msg=Invalid%20IP%20address!');
+	  return;
+  }  
+
+  //ipParts[0] is always base IP
+  //ipParts [1] is port if provided. Otherwise 27015	
+  var ip = ipParts[0];
+  var port = ipParts[1] || 27015;
+
+//Server Defined Vars
+  var createdOn = new Date().getTime() / 1000 >> 0; //in ms. convert to s. bit shift to drop float val.
+  var uniqueKey = randomstring.generate(7);
+
+//Here's all of our variables for the database. Put them into one neat little package
+  var pass_to_db = [ip, port, password, uniqueKey, createdOn];
+
+  db.run("INSERT INTO links (ip,port,password,uniqueKey,createdOn) VALUES(?,?,?,?,?)", pass_to_db, function (err) {
+
+//Build our URL to give to the user
+    var fullLink = "steam://connect/" + ip + ":" + port + "/" + password;
+
+    res.render('pages/index', {output: fullLink, permalink: uniqueKey, msg: null});
+  });
 });
 
 
+//Start on port 3000
 app.listen(3000);
